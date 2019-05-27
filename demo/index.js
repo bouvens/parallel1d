334a99f564c2c497a86a97c75a2ad2140f91c2e9
Alexander Demin <bouvens@gmail.com>
2019-05-27 14:19:28 +0600

    feat: Round time instead of truncation

--- demo/index.js
+++ demo/index.js
@@ -49,7 +49,7 @@ function benchmark() {
       const asyncTime = new Date() - start
       printArray('web workers simplicity checking', result)
       printCalculationTime(asyncTime)
-      const timesFaster = Math.trunc(syncTime / asyncTime * 100) / 100
+      const timesFaster = Math.round(syncTime / asyncTime * 10) / 10
       print(`Parallel calculations were ~${timesFaster} times faster.`)
       showEnd()
     },
