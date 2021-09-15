95ffab06bb9649c12b1f4772722fcba599dea6f2
Alexander Demin <bouvens@gmail.com>
2021-09-15 15:45:04 +0600

    fix: Prevent memory leak in promisified version

--- promisified.js
+++ promisified.js
@@ -1,6 +1,9 @@
 const Parallel = require('.')
 
 module.exports = (worker, workerOptions, jobSize, options) => new Promise((resolve) => {
-  const workers = new Parallel(worker, resolve, options)
+  const workers = new Parallel(worker, (result) => {
+    workers.terminate()
+    resolve(result)
+  }, options)
   workers.start(workerOptions, jobSize)
 })
