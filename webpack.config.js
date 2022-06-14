99b469ab05852d93c519e6cba436eacfa1b8f340
Alexander Demin <bouvens@gmail.com>
2022-06-14 23:05:25 +0300

    feat: Add an icon

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ module.exports = () => {
       path: path.join(__dirname, 'build'),
       publicPath: '',
       filename: 'bundle.js',
+      assetModuleFilename: 'images/[name].[ext]',
     },
     module: {
       rules: [
@@ -27,6 +28,14 @@ module.exports = () => {
         {
           test: /\.css$/,
           use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(html)$/,
+          loader: 'html-loader',
+        },
+        {
+          test: /\.(png|gif|svg|jpe?g)$/,
+          type: 'asset',
         },
       ],
     },
